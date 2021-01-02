d402aa9984cfa698c2d62750fb803b767c89a4ec
vproskurin <vladimir.proskurin@gmail.com>
2021-01-02 11:00:26 -0800

    routing

--- pages/notes/[...params].js
+++ pages/notes/[...params].js
@@ -1,3 +1,5 @@
+// This is a catch all type of page that will get all of ../1/2/3/4 type of pages
+
 import React from 'react'
 import { useRouter } from 'next/router'
 
@@ -6,6 +8,6 @@ export default () => {
   const { params }= router.query
 
   return (
-    <h1>Note: {id} </h1>
+    <h1>Note: {params} </h1>
   )
 }
--- pages/notes/index.js
+++ pages/notes/index.js
@@ -1,3 +1,35 @@
-import React from 'react'
+import React from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
-export default () => <h1>Notes</h1>
+const Page = () => {
+  const notes = new Array(15)
+    .fill(1)
+    .map((e, i) => ({ id: i, title: `Note: ${i}` }));
+
+  const router = useRouter();
+  const id = 2;
+
+  return (
+    <div>
+      <h1>Notes</h1>
+
+      {notes.map((note) => (
+        <div>
+          <Link key={note.id} href="/notes/[id]" as={`/notes/${note.id}`}>
+            <a>
+              <strong>{note.title}</strong>
+            </a>
+          </Link>
+        </div>
+      ))}
+
+      <p>Using router</p>
+      <button onClick={(e) => router.push("/notes/[id]", `/notes/${id}`)}>
+        To Note 2
+      </button>
+    </div>
+  );
+};
+
+export default Page;
