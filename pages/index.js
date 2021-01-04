e64780baf5ffd2fd063d4426ee8882e0875873cf
vproskurin <vladimir.proskurin@gmail.com>
2021-01-03 16:22:36 -0800

    css and theme-ui example

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,6 @@
+/** @jsxRuntime classic /
+/** @jsx jsx */
+import { jsx } from "theme-ui";
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import Link from "next/link";
@@ -10,12 +13,19 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div>
-        <Link href="/notes">
-          <a>
-            Link to Notes
-          </a>
-          </Link>
+      <div sx={{ height: `calc(100vh - 60px)` }}>
+        <div
+          sx={{
+            variant: "containers.page",
+            display: "flex",
+            alignItems: "center",
+            height: "100%",
+          }}
+        >
+          <h1 sx={{ fontSize: 8, my: 0 }}>
+            This is a really dope note taking app.
+          </h1>
+        </div>
       </div>
 
       <main className={styles.main}>
--- pages/notes/index.js
+++ pages/notes/index.js
@@ -1,35 +1,42 @@
-import React from "react";
+/** @jsxRuntime classic /
+/** @jsx jsx */
+import { jsx } from "theme-ui";
 import Link from "next/link";
-import { useRouter } from "next/router";
 
-const Page = () => {
+export default () => {
   const notes = new Array(15)
     .fill(1)
-    .map((e, i) => ({ id: i, title: `Note: ${i}` }));
-
-  const router = useRouter();
-  const id = 2;
+    .map((e, i) => ({ id: i, title: `This is my note ${i}` }));
 
   return (
-    <div>
-      <h1>Notes</h1>
+    <div sx={{ variant: "containers.page" }}>
+      <h1>My Notes</h1>
 
-      {notes.map((note) => (
-        <div>
-          <Link key={note.id} href="/notes/[id]" as={`/notes/${note.id}`}>
-            <a>
-              <strong>{note.title}</strong>
-            </a>
-          </Link>
-        </div>
-      ))}
+      <div
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          flexWrap: "wrap",
+        }}
+      >
+        {notes.map((note) => (
+          <div sx={{ width: "33%", p: 2 }}>
+            <Link key={note.id} href="/notes/[id]" as={`/notes/${note.id}`}>
+              <a sx={{ textDecoration: "none", cursor: "pointer" }}>
+                <div sx={{ variant: "containers.card" }}>
+                  <strong>{note.title}</strong>
+                </div>
+              </a>
+            </Link>
+          </div>
+        ))}
+      </div>
 
       <p>Using router</p>
-      <button onClick={(e) => router.push("/notes/[id]", `/notes/${id}`)}>
+      <button sx={{color: "main"}} onClick={(e) => router.push("/notes/[id]", `/notes/${id}`)}>
         To Note 2
-      </button>
+      </button>  
     </div>
   );
 };
-
-export default Page;
