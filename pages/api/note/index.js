b8d8a313abfeec3d74cc9fa22bd619ce54ccd8fc
vproskurin <vladimir.proskurin@gmail.com>
2021-01-06 23:40:23 -0800

    next builtin API part2

--- pages/api/note/index.js
+++ pages/api/note/index.js
@@ -5,4 +5,11 @@ const handler = nc()
   .get((req, res) => {
     res.json({ data: notes });
   })
-  .post();
+  .post((req, res) => {
+    const id = Date.now();
+    const note = { ...req.body, id };
+
+    notes.push(note);
+    res.json({ data: note });
+  });
+export default handler;
