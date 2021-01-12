ee916d74f95111765187ca4e80a45603e44e6c51
vproskurin <vladimir.proskurin@gmail.com>
2021-01-11 23:39:31 -0800

    data fetching part2

--- pages/notes/[id].js
+++ pages/notes/[id].js
@@ -14,3 +14,22 @@ export default () => {
     </div>
   )
 }
+
+export async function getServerSideProps({params, req, res}) {
+  const response = await fetch(`http://localhost:3000/api/note/${params.id}`)
+
+  // so much power!
+  if (!response.ok) {
+    res.writeHead(302, { Location: '/notes' })
+    res.end()
+    return {props: {}}
+  }
+
+  const {data} = await response.json()
+  
+  if (data) {
+    return {
+      props: {note: data}
+    }
+  }
+}
